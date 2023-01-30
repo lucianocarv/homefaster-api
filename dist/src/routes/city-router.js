9add66eae78ed52c959b62a367ab6202742f6797
Luciano Carvalho <91852629+lucianocarv@users.noreply.github.com>
2023-01-30 18:28:06 -0300

    Delete dist/src directory

--- dist/src/routes/city-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cityRoutes = void 0;
-const city_controller_1 = require("../controllers/city-controller");
-async function cityRoutes(fastify, options) {
-    fastify.get('/cities', city_controller_1.cityController.index);
-    fastify.post('/cities', city_controller_1.cityController.create);
-}
-exports.cityRoutes = cityRoutes;
--- dist/src/routes/province-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.provinceRoutes = void 0;
-const province_controller_js_1 = require("../controllers/province-controller.js");
-async function provinceRoutes(fastify) {
-    fastify.get('/provinces', province_controller_js_1.provinceController.index);
-    fastify.post('/provinces', province_controller_js_1.provinceController.create);
-}
-exports.provinceRoutes = provinceRoutes;
