ff125563ae8d267af0f864cd7db577d8c839768d
Luciano Carvalho <lucianocarv13@gmail.com>
2023-05-27 15:20:39 -0300

    atualizando as rotas do maps e property

--- src/api/routes/maps.auth.routes.ts
+++ src/api/routes/maps.auth.routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { mapsController } from '../controllers/maps.controller';
 
 async function mapsRoutesAuth(fastify: FastifyInstance) {
-  fastify.get('/maps/get-address', mapsController.getAddress);
-  fastify.post('/maps/validate-address', mapsController.validateAddress);
+  fastify.get('/maps/geocode/address', mapsController.getAddress);
+  fastify.post('/maps/validate-address/address', mapsController.validateAddress);
 }
 export { mapsRoutesAuth };
--- src/api/routes/property.auth.routes.ts
+++ src/api/routes/property.auth.routes.ts
@@ -3,6 +3,7 @@ import { propertyController } from '../controllers/property.controller';
 
 async function propertyRoutesAuth(fastify: FastifyInstance) {
   fastify.post('/properties', propertyController.createOneProperty);
+  fastify.post('/properties/input-validate', propertyController.validateEntriesToCreateAProperty);
   fastify.post('/properties/:id/images/upload', propertyController.uploadImage);
   fastify.put('/properties/:id', propertyController.updateOneProperty);
   fastify.delete('/properties/:id', propertyController.deleteOneProperty);
