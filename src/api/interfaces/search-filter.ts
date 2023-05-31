ae8b8fee1b3fed95d39bf481220ef71222bad78a
Luciano Carvalho <lucianocarv13@gmail.com>
2023-05-30 21:30:04 -0300

    Criando interfaces para lidar com os dados recebidos na rota /properties/search

--- src/api/interfaces/search-filter.ts
+++ src/api/interfaces/search-filter.ts
@@ -1,14 +1,29 @@
-export interface IDescriptionFilter {
-  price_min?: number;
-  price_max?: number;
-  bathrooms?: number;
-  badrooms?: number;
-  furnished?: boolean;
-  min_property_area?: number;
-  max_property_area?: number;
-  pets_cats?: number;
-  pets_dogs?: number;
-  smoking?: boolean;
-  type?: number;
-  order?: string;
+export interface PropertyFilterProperties {
+  province: string;
+  city: string;
+  community: string;
+  price_min: number;
+  price_max: number;
+  badrooms: number;
+  bathrooms: number;
+  furnished: boolean;
+  pets_cats: number;
+  pets_dogs: number;
+  smoking: boolean;
+  type_id: number;
+}
+
+export interface PropertyFilterPropertiesController {
+  province: any;
+  city: any;
+  community: any;
+  price_min: any;
+  price_max: any;
+  badrooms: any;
+  bathrooms: any;
+  furnished: any;
+  pets_cats: any;
+  pets_dogs: any;
+  smoking: any;
+  type_id: any;
 }
