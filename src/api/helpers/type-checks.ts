37e76f377ac155ad599ee7fb54cad9df9b754430
Luciano Carvalho <lucianocarv13@gmail.com>
2023-05-30 21:26:27 -0300

    Criando um arquivo com funções que verificam os valores e tipos de dados recebidos na rota /properties/search

---
+++ src/api/helpers/type-checks.ts
@@ -0,0 +1,15 @@
+import { CustomError } from './custom-error';
+
+export function checkThatTheReceivedValueIsNotAnEmptyString(property: any) {
+  if (property == undefined || property == '') return undefined;
+  else return property;
+}
+
+export function checkIfReceivedValueIsNumberOrBoolean(property: any) {
+  if (property !== undefined && isNaN(property)) throw CustomError('_', `A propriedade ${property} não é válida!`, 400);
+  else if (property == undefined) return undefined;
+  else if (property == '') return undefined;
+  else if (property == 'false') return false;
+  else if (property == 'true') return true;
+  else return Number(property);
+}
